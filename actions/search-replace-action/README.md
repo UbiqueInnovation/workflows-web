707f9849d771439ee4d6ea58a09b1fb99b5a1d3d
Marc Widmer <widmer@ubique.ch>
2022-09-30 14:21:57 +0200

    new search-replace action, folder restructure

---
+++ actions/search-replace-action/README.md
@@ -0,0 +1,37 @@
+# Search Replace Action
+
+A action to replace/delete regex-matched patterns in files.
+
+### Usage
+
+Add it to the steps of a workflow.
+The `replacement_file` input is the path to the file specifying the replacements and the optional `dir` input is a path prefix that will be prepended to all file paths specified in the `replacement_file`.
+
+```
+  .
+  .
+  .
+    steps:
+      - uses: "./actions/search-replace-action"
+        with:
+          replacement_file: <replacement_file>
+          dir: "unzipped"
+  .
+  .
+  .
+```
+
+The `replacement_file` should have the following structure:
+
+```
+edits:
+ - file: "./file-to-edit.js"
+   pattern: "regex pattern which sould be replaced"
+   replace: "what the pattern should be replaced with"
+ - file: "./2nd-file-to-edit.js"
+   pattern: "regex pattern which sould be replaced"
+   replace: "what the pattern should be replaced with"
+.
+.
+.
+```
--- actions/search-replace-action/string_replace.py
+++ actions/search-replace-action/string_replace.py
@@ -11,12 +11,12 @@ def perform_edit(filename, pattern, replace):
     file.truncate()
     file.write(file_contents)
 
-REPLACEMENT_FILE = os.getenv('REPLACEMENT_FILE')
-EDITS_DIR = os.getenv('EDITS_DIR')
+REPLACEMENT_FILE = os.getenv('INPUT_REPLACEMENT_FILE')
+EDITS_DIR = os.getenv('INPUT_DIR')
 PREFIX = ''
 
 if REPLACEMENT_FILE == None:
-  raise RuntimeError("Environment variable 'REPLACEMENT_FILE' not specified.")
+  raise RuntimeError("Environment variable 'INPUT_REPLACEMENT_FILE' not specified.")
 
 if EDITS_DIR != None:
   PREFIX = EDITS_DIR
@@ -24,12 +24,12 @@ if EDITS_DIR != None:
 with open(REPLACEMENT_FILE, "r") as stream:
   try:
     yaml_content = yaml.safe_load(stream)
-  except:
-    raise 
+  except yaml.YAMLError as exc:
+    raise RuntimeError("Error parsing YAML file: " + str(exc))
 
 for edit in yaml_content['edits']:
   path = os.path.abspath(os.path.join(PREFIX, edit['file']))
   try:
     perform_edit(path, edit['pattern'], edit['replace'])
-  except:
-    raise
+  except Exception as exc:
+    raise RuntimeError("Error performing edit on file '" + path + "': " + str(exc))
